7cf154d2195e4b07a8b6252cd20e1f5499ea87d9
Henry Ludemann <misc@hl.id.au>
2010-06-11 16:22:42 +0800

    Add copyright headers.

--- fogbugz-to-fogbugz.py
+++ fogbugz-to-fogbugz.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 
+#   Copyright (C) 2010 Henry Ludemann <misc@hl.id.au>
+#
+#   This file is part of the fogbugz import/export library.
+#
+#   The fogbugz import/export library is free software; you can redistribute it
+#   and/or modify it under the terms of the GNU Lesser General Public
+#   License as published by the Free Software Foundation; either
+#   version 2.1 of the License, or (at your option) any later version.
+#
+#   The fogbugz import/export library is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+#   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this library; if not, see
+#   <http://www.gnu.org/licenses/>.
+
+
 import logging
 from optparse import OptionParser
 import sys
--- fogbugz/export.py
+++ fogbugz/export.py
@@ -1,3 +1,21 @@
+#   Copyright (C) 2010 Henry Ludemann <misc@hl.id.au>
+#
+#   This file is part of the fogbugz import/export library.
+#
+#   The fogbugz import/export library is free software; you can redistribute it
+#   and/or modify it under the terms of the GNU Lesser General Public
+#   License as published by the Free Software Foundation; either
+#   version 2.1 of the License, or (at your option) any later version.
+#
+#   The fogbugz import/export library is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+#   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this library; if not, see
+#   <http://www.gnu.org/licenses/>.
+
 from collections import namedtuple
 import copy
 import logging
